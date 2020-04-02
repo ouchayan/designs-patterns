f9006df9257a4db2f5642d928792041aff1bfa2b
ouchayan <ouchayan.h@gmail.com>
2020-04-02 13:29:55 +0200

    Create Singleton-example-1.php

---
+++ Singleton-example-1.php
@@ -0,0 +1,35 @@
+<?php
+/*
+1) Les méthodes et les variables static appartient a la classe et non pas a l'object.
+2) Une propriété statique peut tout à fait changer de valeur au cours du temps à la différence d’une constante dont la valeur est fixée. Simplement, la valeur d’une propriété statique sera partagée par tous les objets issus de la classe dans laquelle elle a été définie.
+3) Une classe avec eun onstructeur privé ne peux être instancié à l'exterieur de la classe.
+4) singleton est un design pattern de la famille création qui assure la création d'une seule instance d'une classe.
+5) new self() et new static.
+*/
+class BdConnexion 
+{
+  private static $instance = null;
+
+  private function __construct()
+  {
+    
+  }
+  
+  public static function getInstance() 
+  {
+     //static $this->instance = null;
+     if (null == self::$instance) {
+         self::$instance = new BdConnexion();
+     } else {
+         echo "Already instanted";
+     }
+     
+     //echo self::$instance;
+  }
+ 
+}
+
+BdConnexion::getInstance();
+BdConnexion::getInstance();
+BdConnexion::getInstance();
+BdConnexion::getInstance();
